66ae2d86f5c6ef4f41ede7dbe89675f9f167aeb8
maxxNcode <markfroilan2nd@gmail.com>
2026-05-22 00:52:37 +0800

    first commit

---
+++ _helpers/send-email.ts
@@ -0,0 +1,21 @@
+import { Resend } from 'resend';
+
+export default async function sendEmail({ to, subject, html, from = process.env.EMAIL_FROM }: any) {
+  const resend = new Resend(process.env.RESEND_API_KEY);
+  try {
+    const { data, error } = await resend.emails.send({
+      from,
+      to: [to],
+      subject,
+      html
+    });
+    if (error) {
+      console.error('Resend API error:', error);
+      throw new Error(error.message);
+    }
+    return data;
+  } catch (error) {
+    console.error('Error sending email:', error);
+    throw error;
+  }
+}
